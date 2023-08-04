00c1493894e170613604a43fbe61f28db84a74c0
Francis Bello <bellofrancis87@gmail.com>
2023-08-04 23:58:48 +0100

    added title to the site

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head > <title>Ultiverse</title></Head>
       <body >
         <Main />
         <NextScript />
